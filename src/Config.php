a2b8ce4b69fbf93aba7c882fa2340715b36e8b34
Rodrigo Gattermann <rodrigo.gattermann@gmail.com>
2018-02-09 22:07:40 -0200

    Ajustada classe de configuração

--- src/Config.php
+++ src/Config.php
@@ -12,36 +12,29 @@ class Config
     public function __construct()
     {
         $this->setNomeEmpresa('UNIPAGO SOLUCOES COBRANCA LTDA');
-        $this->setLocalArquivo(__DIR__ . '/../resources/C12345.RET');
+        $this->setDiretorioArquivos(__DIR__ . '/../resources/');
+        $this->setNomeArquivoRetorno('C12345.RET');
         $this->setOcorrenciasValidas(['06', '09']);
     }
 
+    /**
+     * Transforma o objeto em um array associativo
+     *
+     * @return array
+     */
     public function toArray(): array
     {
         return [
-            'empresa' => $this->empresa,
-            'local_arquivo' => $this->localArquivo,
+            'nomeEmpresa' => $this->getNomeEmpresa(),
+            'nomeArquivoRetorno' => $this->getDiretorioArquivos(),
+            'diretorioArquivos' => $this->getNomeArquivoRetorno(),
+            'ocorrenciasValidas' => $this->getOcorrenciasValidas(),
         ];
     }
 
-    /**
-     * Get the value of ocorrenciasValidas
-     */
-    public function getOcorrenciasValidas()
+    public function getFullPathFile()
     {
-        return $this->ocorrenciasValidas;
-    }
-
-    /**
-     * Set the value of ocorrenciasValidas
-     *
-     * @return  self
-     */
-    public function setOcorrenciasValidas($ocorrenciasValidas)
-    {
-        $this->ocorrenciasValidas = $ocorrenciasValidas;
-
-        return $this;
+        return $this->getDiretorioArquivos() . $this->getNomeArquivoRetorno();
     }
 
     /**
@@ -65,21 +58,61 @@ class Config
     }
 
     /**
-     * Get the value of localArquivo
+     * Get the value of nomeArquivoRetorno
      */
-    public function getLocalArquivo()
+    public function getNomeArquivoRetorno()
     {
-        return $this->localArquivo;
+        return $this->nomeArquivoRetorno;
     }
 
     /**
-     * Set the value of localArquivo
+     * Set the value of nomeArquivoRetorno
      *
      * @return  self
      */
-    public function setLocalArquivo($localArquivo)
+    public function setNomeArquivoRetorno($nomeArquivoRetorno)
     {
-        $this->localArquivo = $localArquivo;
+        $this->nomeArquivoRetorno = $nomeArquivoRetorno;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of diretorioArquivos
+     */
+    public function getDiretorioArquivos()
+    {
+        return $this->diretorioArquivos;
+    }
+
+    /**
+     * Set the value of diretorioArquivos
+     *
+     * @return  self
+     */
+    public function setDiretorioArquivos($diretorioArquivos)
+    {
+        $this->diretorioArquivos = $diretorioArquivos;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of ocorrenciasValidas
+     */
+    public function getOcorrenciasValidas()
+    {
+        return $this->ocorrenciasValidas;
+    }
+
+    /**
+     * Set the value of ocorrenciasValidas
+     *
+     * @return  self
+     */
+    public function setOcorrenciasValidas($ocorrenciasValidas)
+    {
+        $this->ocorrenciasValidas = $ocorrenciasValidas;
 
         return $this;
     }
